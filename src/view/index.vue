9acaa7d27ef88386434f3716d335823e185742f9
guodongyin <15923429718@163.com>
2018-09-04 19:50:57 +0800

    09-04

---
+++ src/view/index.vue
@@ -0,0 +1,15 @@
+<style lang="scss" type="text/scss" scoped>
+  .index{
+    width:200px;
+    height:200px;
+  }
+</style>
+<template>
+  <div class="index">
+
+  </div>
+</template>
+
+<script>
+
+</script>
